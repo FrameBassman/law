3f68369d4a4a7b2b53d3725e0d513a29bdc4b3f3
Dmitry Romashov <framebassman@gmail.com>
2021-01-29 09:57:28 +0300

    initial

---
+++ Law.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using DocumentFormat.OpenXml.Drawing;
+using DocumentFormat.OpenXml.Packaging;
+
+namespace Law.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.SearchAndReplace("test.docx");
+        }
+
+        // To search and replace content in a document part.
+        public void SearchAndReplace(string document)
+        {
+            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(document, true))
+            {
+                string docText = null;
+                using (StreamReader sr = new StreamReader(wordDoc.MainDocumentPart.GetStream()))
+                {
+                    docText = sr.ReadToEnd();
+                }
+
+                Regex regexText = new Regex("%PLACEHOLDER%");
+                docText = regexText.Replace(docText, "Hi Everyone!");
+
+                using (StreamWriter sw = new StreamWriter(wordDoc.MainDocumentPart.GetStream(FileMode.Create)))
+                {
+                    sw.Write(docText);
+                }
+            }
+        }
+    }
+}
